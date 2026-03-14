8ca4403a4c1b87998e9ac8b25e0abc88f894e127
Sparsh Pandey <sparshpandey06@gmail.com>
2026-03-14 18:09:20 -0400

    Last removal of buggy submodules

---
+++ buggy/bug-71/app/src/main/java/fr/neamar/kiss/searcher/QueryInterface.java
@@ -0,0 +1,12 @@
+package fr.neamar.kiss.searcher;
+
+import fr.neamar.kiss.ui.ListPopup;
+
+public interface QueryInterface {
+    void temporarilyDisableTranscriptMode();
+    void updateTranscriptMode(int transcriptMode);
+
+    void launchOccurred();
+
+    void registerPopup(ListPopup popup);
+}
